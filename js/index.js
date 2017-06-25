7b00d38dcf3c71eb625925f0303912f1815cd459
shishunlin <1160916277@qq.com>
2017-06-25 21:17:10 +0800

    这是第三次修改

--- js/index.js
+++ js/index.js
@@ -2,5 +2,6 @@
 	$(function(){
 		alert("这个第一次修改")
 		console.log("这个第二次修改")
+		console.log("这是第三次修改")
 	})
 <script/>
--- js/moban.js
+++ js/moban.js
@@ -1,4 +1,5 @@
 <script>
 	var h1 = document.getElementById("h1")
 	console.log("这是第二次修改");
+	console.log("这是第三次修改")
 </script>
